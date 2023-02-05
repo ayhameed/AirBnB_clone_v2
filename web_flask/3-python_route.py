73037aee77c2f91a0943977135ea9de5dcb024ed
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2023-02-05 23:44:10 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""Starts Flask web app
+Routes:
+    / - display "Hello HBNB!"
+    /hbnb - display "HBNB"
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+"""
 from flask import Flask
 
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello():
-    """Displays welcome message"""
+@app.route('/', strict_slashes=False)
+def hbnb_route():
+    """prints Hello HBNB"""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb')
-def hello_hbnb():
-    """Displays strict with specified route"""
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """prints HBNB"""
     return "HBNB"
 
 
-@app.route('/c/<text>')
-def hello_c(text):
-    """Display 'C' to the screen"""
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_text(text):
+    """prints C followed by <text> content"""
     text = text.replace("_", " ")
     return "C %s" % text
 
 
-@app.route('/python')
-@app.route('/python/<text>')
-def python_route(text):
-    """Displays Python followed by route"""
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """prints Python is cool"""
     text = text.replace("_", " ")
     return "Python %s" % text
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
